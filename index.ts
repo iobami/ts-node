8c94398a1322201da440da557ba11c9c7d196c1e
iobami <ayobamialadenoye@yahoo.com>
2021-04-01 21:55:32 +0100

    render html

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, './src/client')));
+app.use(express.json());
 
 app.get('/', ({},res) => res.send('Express + TypeScript Server'));
 
