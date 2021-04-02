5518995b9b6354d12619ca72d867fe802fd932e9
iobami <ayobamialadenoye@yahoo.com>
2021-04-02 13:20:08 +0100

    js complie fix

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+const env = dotenv.config();
+dotenvExpand(env);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './src/client')));
 app.use(express.json());
 
-app.get('/', ({},res) => res.send('Express + TypeScript Server'));
-
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running on PORT: ${PORT}`);
 });
